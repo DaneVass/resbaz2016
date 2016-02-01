087eaafc4a6b5ef6a65bf3554ccd40f2d34bb6e4
Dane Vassiliadis <dane.vassiliadis@gmail.com>
2016-02-02 10:56:02 +1100

    playing with canvas scg elements

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
 var dataUrl = "https://raw.githubusercontent.com/IsaKiko/D3-visualising-data/gh-pages/code/nations.json";
 d3.json(dataUrl, function(nations) { 
-
-var chart_area = d3.select('#chart_area');
-
-var frame = chart_area.append("svg");
-
-var canvas = frame.append("g");
-
-//set margins, width and height
-//can change css attributes of the frame, such as its width, within js using d3
-var margin = {top: 19.5, right: 19.5, bottom: 19.5, left: 39.5};
-var frame_width = 960;
-var frame_height = 350;
-var canvas_width = frame_width - margin.left - margin.right;
-var canvas_height = frame_height - margin.top - margin.bottom;
-
-
-
-frame.attr("width", frame_width)
+  // Select the chart area by ID 
+        var chart_area = d3.select("#chart_area");
+        // add a SVG picture frame
+        var frame = chart_area.append("svg");
+        // Create canvas inside frame.
+        var canvas = frame.append("g");
+        // create circle within g
+        var circles = canvas.append("circle")
+        // set dimensions of the frame
+        var margin = {top: 19.5, right: 19.5, bottom: 19.5, left: 39.5};
+        var frame_width = 960;
+        var frame_height = 350;
+        var canvas_width = frame_width - margin.left - margin.right;
+        var canvas_height = frame_height - margin.top - margin.bottom;
+        // Set frame attributes width and height.
+        frame.attr("width", frame_width);
+        frame.attr("height", frame_height);
+        // Shift the canvas and make it slightly smaller than the svg canvas.
+        canvas.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        //
+        circles.attr("cx",25).attr("cy",25).attr("fill","green").attr("stroke", "black").attr("r", 40).attr("stroke-width", "4px");
 
 })
 
